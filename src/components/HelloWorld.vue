5065d91e7bad6918ad8e12ad5412add1d69abcf0
liuzijian <liuzijian@gaohangip.com>
2020-09-08 16:50:23 +0800

    test build images

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      check out the111
       <a href="#" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <ul>
